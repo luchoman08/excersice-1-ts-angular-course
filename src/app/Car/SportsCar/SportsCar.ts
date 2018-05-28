de1e85365bc7b16ddba9ab95e1fad44d49d9eb49
Luis Gerardo Manrique <luis.manrique@correounivalle.edu.co>
2018-05-28 13:02:05 -0500

    Sports car has spoilerattr added

--- src/app/Car/SportsCar/SportsCar.ts
+++ src/app/Car/SportsCar/SportsCar.ts
@@ -6,8 +6,27 @@ export abstract class SportsCar extends Car {
     protected static standardAceleration: number = 0.6; //k / h * h
     protected static standardNumberOfDoors: number = 2;
     protected static standardCapacity: number = 2;
+
+   
+    private _hasSpoiler: boolean;
     constructor(modelName: string, color: Colors, aceleration: number, numberOfDoors: number,
-        maxSpeed: number, capacity: number, isConvertible: boolean) {
+        maxSpeed: number, capacity: number, isConvertible: boolean, hasSpoiler: boolean) {
             super(modelName, color, aceleration, numberOfDoors, maxSpeed, capacity, isConvertible);
+            this._hasSpoiler = hasSpoiler;
         }
+    /**
+     * Getter hasSpoiler
+     * @return {boolean}
+     */
+	public get hasSpoiler(): boolean {
+		return this._hasSpoiler;
+	}
+
+    /**
+     * Setter hasSpoiler
+     * @param {boolean} value
+     */
+	public set hasSpoiler(value: boolean) {
+		this._hasSpoiler = value;
+	}
 }
